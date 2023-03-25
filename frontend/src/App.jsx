7348001f1a41b232d6e78504e151d4b5826f388a
Denis <chainsawfish@gmail.com>
2023-03-26 01:18:26 +0700

    feat days with event now visual different from others

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import {useState} from "react";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
 import EventsList from "./components/EventsList.jsx";
 import EditForm from "./components/EditForm.jsx";
+import {useSelector} from "react-redux";
 
 function App() {
     const [date, setDate] = useState(new Date())
     const [currentDay, setCurrentDay] = useState(null)
     const [showEdit, setShowEdit] = useState(false)
-
+    const events = useSelector(state => state.calendarEvents.events)
     const handleDayClick = (value) => {
         setCurrentDay(value)
     }
@@ -17,10 +17,22 @@ function App() {
         setShowEdit(true)
     }
 
+    const hasEvents = ({date}) => {
+        const validArray = events.filter(obj => {
+            if (obj.date === date.toLocaleDateString()) {
+                console.log(obj.date, '===' ,date.toLocaleDateString())
+                return 1
+            }
+
+        })
+        console.log(validArray)
+        return validArray.length ? <p className="text-3xl text-green-600 relative top-[-10px]">*</p> : null
+    }
+
     return (
         <>
             <div className="flex flex-row justify-center gap-8 mt-10">
-                <Calendar onChange={setDate} value={date} onClickDay={handleDayClick}/>
+                <Calendar onChange={setDate} value={date} onClickDay={handleDayClick} tileContent={hasEvents}/>
                 {currentDay && <EventsList currentDay={date}/>}
             </div>
             <div className="flex flex-col items-center">
